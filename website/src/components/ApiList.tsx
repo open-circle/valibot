4cf4e9e6d1e722eea5cb628a3e02cc7cf045a2d3
Fabian Hiller <hillerfabian11@gmail.com>
2025-11-22 23:19:42 -0500

    Refactor class order in various components via Prettier

--- website/src/components/ApiList.tsx
+++ website/src/components/ApiList.tsx
@@ -10,7 +10,7 @@ type ApiListProps = {
  * List to display APIs and navigate to their documentation.
  */
 export const ApiList = component$<ApiListProps>(({ label, items }) => (
-  <ul class="ml-8! lg:ml-10! flex list-none flex-row flex-wrap gap-2">
+  <ul class="ml-8! flex list-none flex-row flex-wrap gap-2 lg:ml-10!">
     {label && label + ': '}
     {items.map((item, index) => (
       <li key={item} class="p-0!">
