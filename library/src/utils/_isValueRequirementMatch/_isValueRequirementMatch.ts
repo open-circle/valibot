d6b87da869c1a25aa320b42e2e2cc02badf26160
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2026-08-03 22:59:53 +0200

    refactor: implement _isValueRequirementMatch utility and update related functions to reuse logic

---
+++ library/src/utils/_isValueRequirementMatch/_isValueRequirementMatch.ts
@@ -0,0 +1,23 @@
+import type { ValueInput } from '../../actions/types.ts';
+import { _isSameValueZero } from '../_isSameValueZero/index.ts';
+
+/**
+ * Checks whether a value matches a value action requirement.
+ *
+ * @param requirement The value action requirement.
+ * @param value The value to check.
+ *
+ * @returns Whether the value matches the requirement.
+ *
+ * @internal
+ */
+// @__NO_SIDE_EFFECTS__
+export function _isValueRequirementMatch(
+  requirement: ValueInput,
+  value: ValueInput
+): boolean {
+  return (
+    _isSameValueZero(requirement, value) ||
+    (requirement <= value && requirement >= value)
+  );
+}
