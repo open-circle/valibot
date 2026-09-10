ff4fdd31f006e789c1de5032199689a82ad2ba9e
Yaroslav Slepukhin <yaroslavslepukhin@gmail.com>
2026-09-10 10:18:07 +0200

    perf: optimize Luhn and ISBN validation

--- library/src/utils/_isLuhnAlgo/_isLuhnAlgo.ts
+++ library/src/utils/_isLuhnAlgo/_isLuhnAlgo.ts
@@ -1,9 +1,4 @@
 /**
- * Non-digit regex.
- */
-const NON_DIGIT_REGEX = /\D/gu;
-
-/**
  * Checks whether a string with numbers corresponds to the luhn algorithm.
  *
  * @param input The input to be checked.
@@ -14,19 +9,19 @@ const NON_DIGIT_REGEX = /\D/gu;
  */
 // @__NO_SIDE_EFFECTS__
 export function _isLuhnAlgo(input: string): boolean {
-  // Remove any non-digit chars
-  const number = input.replace(NON_DIGIT_REGEX, '');
-
   // Create necessary variables
-  let length = number.length;
+  let index = input.length;
   let bit = 1;
   let sum = 0;
 
   // Calculate sum of algorithm
-  while (length) {
-    const value = +number[--length];
-    bit ^= 1;
-    sum += bit ? [0, 2, 4, 6, 8, 1, 3, 5, 7, 9][value] : value;
+  while (index) {
+    const charCode = input.charCodeAt(--index);
+    if (charCode >= 48 && charCode <= 57) {
+      const value = charCode - 48;
+      bit ^= 1;
+      sum += bit ? (value > 4 ? value * 2 - 9 : value * 2) : value;
+    }
   }
 
   // Return whether its valid
